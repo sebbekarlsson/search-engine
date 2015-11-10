a2d98469552204bd3cd743d3980c27483f9b6784
= <sebbekarlsson97@gmail.com>
2015-11-10 21:15:34 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import os
+from setuptools import setup
+
+
+setup(
+    name='searchengine',
+    version='1.0',
+    description='Search Engine',
+    author='Sebastian Robert Karlsson',
+    author_email='sebbekarlsson97@gmail.com',
+    url='http://www.ianertson.com/',
+    install_requires=[
+        'flask',
+        'flask-wtf',
+        'lxml'
+    ]
+)
